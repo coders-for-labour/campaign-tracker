7f2bd42c3efc815add56aa13fe4b344173f073fc
Tom H <tom@codersforcorbyn.com>
2017-05-12 21:34:34 +0100

    Added location type, markers now display on map

---
+++ src/app/location.ts
@@ -0,0 +1,6 @@
+export class Location {
+  name: string;
+  address: string;
+  date: string;
+  image: string;
+}
---
+++ src/app/locations.ts
@@ -0,0 +1,137 @@
+import { Location } from './location';
+
+export const LOCATIONS: Location[] = [
+    {
+        name: "",
+        date: "2017-04-18",
+        address: "Croydon",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-04-20",
+        address: "Westminster",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-04-21",
+        address: "Swindon",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-04-21",
+        address: "Bristol",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-04-21",
+        address: "Cardiff",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-04-22",
+        address: "Warrington",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-04-22",
+        address: "Crewe",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-04-23",
+        address: "Wood Green, London",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-04-24",
+        address: "Aviemore",
+        image: "",
+
+    },
+    {
+        name: "",
+        date: "2017-04-24",
+        address: "Dunfermline",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-04-26",
+        address: "Lambeth, London",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-04-27",
+        address: "Harlow",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-04-29",
+        address: "Bethnal Green and Bow",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-05-01",
+        address: "Battersea, London",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-05-02",
+        address: "Southampton",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-05-03",
+        address: "Bedford",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-05-04",
+        address: "Oxford",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-05-05",
+        address: "Liverpool Wavertree",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-05-05",
+        address: "Manchester Central",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-05-06",
+        address: "Leicester East",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-05-08",
+        address: "Worcester",
+        image: "",
+    },
+    {
+        name: "",
+        date: "2017-05-09",
+        address: "Stretford and Urmston",
+        image: "",
+    }
+];
